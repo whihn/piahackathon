d94c37f0898ee839065da8b072e512d1aad3bcd0
Werner Hihn <hihn@econda.de>
2019-03-22 00:17:31 +0100

    hooray, we have an array

--- src/main/java/de/pia/hackathon/HackathonApplication.java
+++ src/main/java/de/pia/hackathon/HackathonApplication.java
@@ -4,10 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @Controller
@@ -23,41 +19,8 @@ public class HackathonApplication {
         return "index";
     }
 
-    @GetMapping("/product")
-    public TschiboProduct getProduct(@RequestParam byte[] image) {
-
-        return new TschiboProduct();
-    }
-
-    @GetMapping("/image")
-    public byte[] getImage(@RequestParam String imageId) {
-
-        return new byte[0];
-    }
-
-    @GetMapping("/toplist")
-    public List<TschiboProduct> topList() {
-
-        return new ArrayList<>();
-    }
-
-    @GetMapping("/communitylist")
-    public List<TschiboProduct> communitylist() {
-
-        return new ArrayList<>();
-    }
-
-    @GetMapping("/topRated")
-    public List<TschiboProduct> topRated() {
-
-        return new ArrayList<>();
-    }
 }
 
 
 
 
-class TschiboProduct {
-    String name;
-    Double price;
-}
--- src/main/java/de/pia/hackathon/SecondBeanRestController.java
+++ src/main/java/de/pia/hackathon/SecondBeanRestController.java
@@ -2,7 +2,10 @@ package de.pia.hackathon;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @RestController
 @RequestMapping("/api")
@@ -15,6 +18,54 @@ public class SecondBeanRestController {
         // ask solar
 
         return new TchiboProfile();
+    }
+
+    @GetMapping("/product")
+    public TchiboProduct getProduct(@RequestParam byte[] image) {
+
+        return new TchiboProduct();
+    }
+
+    @GetMapping("/image")
+    public byte[] getImage(@RequestParam String imageId) {
+
+        return new byte[0];
+    }
+
+    @GetMapping("/toplist")
+    public List<TchiboProduct> topList() {
+        TchiboProduct product1 = new TchiboProduct();
+        product1.name = "p1";
+        product1.price = 9.90;
+        TchiboProduct product2 = new TchiboProduct();
+        product2.name = "p2";
+        product2.price = 4.99;
+        return asList(product2, product2);
+    }
+
+    @GetMapping("/communitylist")
+    public List<TchiboProduct> communitylist() {
+
+        return new ArrayList<>();
+    }
+
+    @GetMapping("/topRated")
+    public List<TchiboProduct> topRated() {
+
+        return new ArrayList<>();
+    }
+
+}
+class TchiboProduct {
+    String name;
+    Double price;
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getName() {
+        return name;
     }
 }
 
